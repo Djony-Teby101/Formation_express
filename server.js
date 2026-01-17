48228542e0c344331bf4b40679cd6ddeecf4b7cc
teby lorince <111266131+Djony-Teby101@users.noreply.github.com>
2026-01-17 18:15:10 +0100

    Fake database envoie de données.

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const path= require('path');
 
 
 const app= express();
+// / Initier les fichier static
+// app.use(express.static(path.join(__dirname, 'public')))
 
+// Fake database
+let posts=[
+    {id:1, title:'post one', created_at:Date.now()},
+    {id:2, title:'post one', created_at:Date.now()},
+    {id:3, title:'post one', created_at:Date.now()}
+];
 
-
-// Injection de fichier static.
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'))
+app.get('/api/post', (req, res)=>{
+    res.status(200).json(posts);
 })
 
-app.get('/about', (req, res)=>{
-    res.sendFile(path.join(__dirname,'public','about.html'))
-})
+
+
 
 
 app.listen(8000, ()=> console.log('server lancée sur le post: 8000'))
